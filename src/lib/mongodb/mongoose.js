bc66a1994ff0b78ac5f5352be253a8112023f647
Pavan Gupta <pavangpay05@gmail.com>
2024-09-01 19:52:57 +0530

    Add mongoDB and mongoose to save user data from clerk to MongoDB database

---
+++ src/lib/mongodb/mongoose.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+
+let initialized = false;
+
+export const connect = async () => {
+    mongoose.set('strictQuery',true);
+    if(initialized){
+        console.log("MongoDb already connected")
+        return;
+    }
+    try {
+        await mongoose.connect(process.env.MONGODB_URI,{
+            dbName:"next auth app",
+            useNewUrlParser: true,
+            useUnifiedTopology:true
+        });
+        console.log("MongoDB connnected")
+        initialized = true
+    } catch (error) {
+        console.log("MongoDB connection error",error)
+    }
+}
